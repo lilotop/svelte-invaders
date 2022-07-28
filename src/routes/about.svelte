743cbc54c8e58f03fd0bc5bbc948ab8ca3fcbd1b
lilotop <liloelia@gmail.com>
2022-07-29 02:13:51 +0300

    missing files

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,12 @@
-<h1>About Svelte Invaders</h1>
-<h3>Created by Lilo Elia</h3>
+<div class="h-full w-full bg-green-900 text-yellow-200 flex flex-col items-center justify-center">
+	<div class="text-9xl mb-5">👾</div>
+	<div class="text-6xl mb-3">Svelte Invaders</div>
+	<p class="text-lg">
+		This fully functional game is a homage to the original 80s arcade game "Space Invaders"
+	</p>
+	<p class="text-lg">It was created by me as a learning experience of Svelte & SvelteKit</p>
+	<p class="text-lg mt-5">
+		Source code can be found
+		<a class="text-orange-400 underline" href="https://github.com/lilotop/svelte-invaders">here</a>.
+	</p>
+</div>
